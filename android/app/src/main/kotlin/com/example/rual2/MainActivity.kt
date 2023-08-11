affbafedfbb6c37964be8da55a5494566ee2eaef
nahid khan <nahidkhan5567@gmail.com>
2023-08-11 23:44:52 +0600

    first commit

---
+++ android/app/src/main/kotlin/com/example/rual2/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.rual2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,99 @@
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  TextEditingController textController = TextEditingController();
+  late SharedPreferences _prefs;
+
+  String _name = 'rual upgraded';
+  getField(String value) async {
+    final prefs = await SharedPreferences.getInstance();
+    prefs.setString("NAME", _name);
+
+    setState(() {
+      _name = textController.value.text;
+    });
+  }
+
+  loadData() async {
+    _prefs = await SharedPreferences.getInstance();
+    setState(() {
+      _name = _prefs.getString("NAME")!;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text("This is $_name"),
+            TextField(
+              controller: textController,
+              onSubmitted: (value) {
+                getField(value);
+              },
+            ),
+            TextButton.icon(
+                onPressed: () => () async {
+                      final prefs = await SharedPreferences.getInstance();
+                      prefs.remove("NAME");
+
+                      setState(() {});
+                    },
+                icon: const Icon(Icons.remove_done),
+                label: const Text("Remove data"))
+          ],
+        ),
+      ),
+      floatingActionButton: const FloatingActionButton(
+        onPressed: null,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
